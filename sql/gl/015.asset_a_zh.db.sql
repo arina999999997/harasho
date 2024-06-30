7d4f2b5c15ed1162509ce264be8a0f2a0de74981
Arina Aria <arina999999997@gmail.com>
2024-06-30 18:29:29 +0700

    Added placeholder for ko and zh daily theater

---
+++ sql/gl/015.asset_a_zh.db.sql
@@ -0,0 +1,11 @@
+-- This is the asset for the icons for daily theater
+-- We can make a new pack but we can just reuse from jp's pack for now
+INSERT INTO texture (asset_path, pack_name, head, size, key1, key2) VALUES (";c}", "ubias7", "0", "547", "625232640", "1360541736");
+INSERT INTO texture (asset_path, pack_name, head, size, key1, key2) VALUES ("X""I", "ubias7", "547", "551", "1575450016", "486001896");
+
+INSERT INTO m_asset_pack (pack_name, auto_delete) VALUES ("ubias7", "0");
+
+INSERT INTO m_asset_package_mapping (package_key, pack_name, file_Size, metapack_offset, category) VALUES ("main", "ubias7", "394966", "0", "8");
+
+-- This is just a random hash, ideally we would use some hashing algorithm, and it will automatically pack
+UPDATE m_asset_package SET version = "28ef7d2212eb92fb0660e3f8a6266cba8d8c8fd4", pack_num = "14327" WHERE package_key = "main";
